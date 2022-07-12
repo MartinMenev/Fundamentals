5a0956ca220bf5560dc4908f54f51c0892538d3f
MartinMenev <martin.menew@gmail.com>
2022-07-12 12:45:22 +0300

    Adding new completed task

---
+++ src/TextProcessing/P06ReplaceRepeatingChars.java
@@ -0,0 +1,24 @@
+package TextProcessing;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class P06ReplaceRepeatingChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> input = Arrays.stream(scanner.nextLine().split(""))
+        		.collect(Collectors.toList());
+
+        for (int i = 0; i < input.size() -1; i++) {
+            if (input.get(i).equals(input.get(i+1))){
+                input.remove(i);
+                i--;
+            }
+        }
+        for (String item : input) {
+            System.out.print(item);
+        }
+    }
+}
